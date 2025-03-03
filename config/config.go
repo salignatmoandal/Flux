80526db690701f032562eefa3644100c6ed0e73a
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-03-03 20:12:20 +0100

    update config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+)
+
 type Config struct {
 	Server struct {
 		Port string `yaml:"port"`
@@ -19,4 +23,26 @@ type Config struct {
 			TenantID       string `yaml:"tenant_id"`
 		} `yaml:"azure"`
 	} `yaml:"cloud"`
+	Analysis struct {
+		ResourceThreshold float64 `yaml:"resource_threshold"`
+		ScanInterval      string  `yaml:"scan_interval"`
+	} `yaml:"analysis"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
